81ee3d0a63e6567aa620a7cd88bf20e1eb99d500
Shreyanshi10 <200106066@hbtu.ac.in>
2023-09-06 19:57:57 +0530

    Initial commit

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Nulla, ut totam optio tenetur vel unde maiores dignissimos laborum deleniti 
+         tempore necessitatibus odio nihil reprehenderit ipsum mollitia,
+         placeat, velit qui quaerat!</p>
+    </div>
+  )
+}
+
+export default About
